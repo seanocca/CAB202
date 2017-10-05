a5e9e4dfa69e280c3de45a7824ad8e813352cec1
seanocca <32288543+seanocca@users.noreply.github.com>
2017-10-05 15:52:40 +0100

    Movement into its own file
    Moved movement into its own file

--- Ass2/Assignment_2.c
+++ Ass2/Assignment_2.c
@@ -16,6 +16,7 @@
 #include "char.h"
 #include "collision.h"
 #include "initiliaze.h"
+#include "movement.h"
 
 #define FREQ (8000000.0)
 #define PRESCALE (1024.0)
@@ -346,115 +347,6 @@ void key_follow(void){
 
 	sprite_draw(&key);
 
-}
-
-void move_player(void){
-	player.is_visible = 1;
-
-	//object movement is inversed to show correct player movement;
-
-	//UP
-	if (BIT_IS_SET(PIND, 1)){
-		if (top.y + top.height <= 2 && player.y + round(PLAYERHEIGHT / 2) + 1 == LCD_Y / 2){
-			tower.y++;
-			key.y++;
-			left.y++;
-			right.y++;
-			top.y++;
-			bottom.y++;
-			door.y++;
-			enemy.y++;
-		} else {
-			if (wall_collision_movement(player) == 0){
-				player.y--;
-			}
-		}
-		
-	} else 
-	//DOWN
-	if (BIT_IS_SET(PINB, 7)){
-		if (bottom.y > LCD_Y - 2 && player.y + round(PLAYERHEIGHT / 2)  + 1 == LCD_Y / 2){
-			tower.y--;
-			key.y--;
-			left.y--;
-			right.y--;
-			top.y--;
-			bottom.y--;
-			door.y--;
-			enemy.y--;
-		} else{
-			if (wall_collision_movement(player) == 0){
-				player.y++;
-			}
-		}
-	} else
-	//LEFT
-	if (BIT_IS_SET(PINB, 1)){	
-		if (left.x + left.width  < 2 && player.x + round(PLAYERWIDTH / 2) + 1 == LCD_X / 2){
-			tower.x++;
-			key.x++;
-			left.x++;
-			right.x++;
-			top.x++;
-			bottom.x++;
-			door.x++;
-			enemy.x++;
-		} else {
-			if (wall_collision_movement(player) == 0){
-				player.x--;
-			}
-		}
-		
-	} else
-	//RIGHT
-	if (BIT_IS_SET(PIND, 0)){
-		if (right.x + right.width > LCD_X - 2 && player.x + round(PLAYERWIDTH / 2) + 1 == LCD_X / 2){
-			tower.x--;
-			key.x--;
-			left.x--;
-			right.x--;
-			top.x--;
-			bottom.x--;
-			door.x--;
-			enemy.x--;
-		} else {
-			if (wall_collision_movement(player) == 0){
-				player.x++;
-			}
-		}
-	}
-
-	if (player.is_visible == 1){
-		sprite_draw(&player);
-	}
-
-}
-
-void enemy_crawl(void){
-	int play_x = player.x, play_y = player.y;
-
-	if (sprite_collision(player, enemy) == 1){
-		lives--;
-
-		sprite_init(&player, round((LCD_X - PLAYERWIDTH) / 2), round((LCD_Y - PLAYERHEIGHT) / 2), PLAYERWIDTH, PLAYERHEIGHT, playerBitmaps);
-	}
-
-	if (enemy.x <= 83 && enemy.x >= 0){
-		if (enemy.y <= 47 && enemy.y >= 0){
-			if (enemy.x < play_x){
-				enemy.x += 0.1;
-			} 
-			if (enemy.x > play_x){
-				enemy.x -= 0.1;
-			}
-			if (enemy.y < play_y){
-				enemy.y += 0.1;
-			}
-			if (enemy.y > play_y){
-				enemy.y -= 0.1;
-			}
-		}
-	}
 }
 
 void serial_outputs(void){
